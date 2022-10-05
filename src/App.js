b84e4c576b0a4a0d2dfb7f1ebe5d7b8b53edfb8e
s-polat <spolatde@gmail.com>
2022-10-05 23:24:13 +0200

    useContext fangt an benutzt zu werden

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import { DataStore } from "./DataStore";
 
 function App() {
+
+  const [ events, setEvents ] = useState([]);
+
+
+  const getEvents = async () => {
+ const result = await fetch(
+    `https://tlv-events-app.herokuapp.com/events/uk/london`
+  ).then((data) => data.json());
+  setEvents(result);}
   return (
-    <div>
-     Hello World
-    </div>
+    <DataStore.Provider>
+      <div>
+        <h1>Events</h1>
+        <button onClick={getEvents}>Get Events</button>
+        <ul>
+          {events?.map((event) => (
+            <li key={event.id}>
+              <a href={event.venue.direction}>
+              {`${event.venue.name} - ${event.artists[0]?.name}`}
+              </a>
+              </li>
+          ))}
+        </ul>
+      </div>
+    </DataStore.Provider>
   );
 }
 
